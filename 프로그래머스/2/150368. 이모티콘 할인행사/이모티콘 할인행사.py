28dab3108bf41195251a26f2acc765a6d401e88f
Yein Lee (이예인) <39490214+yein-lee@users.noreply.github.com>
2023-11-10 15:10:32 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 0.09 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.py
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.py
@@ -1,42 +1,37 @@
 def solution(users, emoticons):
     
     discount_rate = [10, 20, 30, 40]
-    emoticon_discount_rates = []
-    results = [] # [가입자수, 판매액, index]
+    discount_rate_comb = []
+    results = [] 
     
-    dfs(0, len(emoticons), [], emoticon_discount_rates, discount_rate)
-    # print(len(emoticon_discount_rates))
-    # print(emoticon_discount_rates)
+    dfs(len(emoticons), [], discount_rate_comb, discount_rate)
     
-    for i, emoticon_discount_rate in enumerate(emoticon_discount_rates):
-        results.append([0, 0, i])
-        for j, user in enumerate(users):
-            user_price = 0
-            for k, emoticon_price in enumerate(emoticons):
-                if emoticon_discount_rate[k] >= user[0]:
-                    user_price += emoticon_price * (1 - emoticon_discount_rate[k] * 0.01)
-                    # print("buy", emoticon_discount_rate[k], user[0], emoticon_price, emoticon_price * (1 - emoticon_discount_rate[k] * 0.01), user_price)
-            if user_price >= user[1]:
-                # 이모티콘 플러스 서비스 가입
+    for comb in discount_rate_comb:
+        results.append([0, 0])
+        for r, max_total in users:
+            total = sum(
+                price * (1 - discount/100.0)
+                for price, discount in zip(emoticons, comb)
+                if discount >= r
+            )
+            if total >= max_total:
                 results[-1][0] += 1
             else:
-                results[-1][1] += user_price
+                results[-1][1] += total
                     
     results.sort(reverse=True)
-    # print(results)
-    # print(emoticon_discount_rates[results[0][2]])
-    
-    return results[0][:2]
+
+    return results[0]
                 
     
-def dfs(t, n, arr, emoticon_discount_rates, discount_rate):
+def dfs(n, arr, discount_rate_comb, discount_rate):
     if len(arr) == n:
-        emoticon_discount_rates.append(arr[:])
+        discount_rate_comb.append(arr[:])
         return
 
     for i in range(len(discount_rate)):
         new_arr = arr[:]
         new_arr.append(discount_rate[i])
-        dfs(i, n, new_arr, emoticon_discount_rates, discount_rate)
+        dfs(n, new_arr, discount_rate_comb, discount_rate)
 
     
