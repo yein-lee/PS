269fb36d5135a950f4814b9cd1275025a86ac68f
Yein Lee (이예인) <39490214+yein-lee@users.noreply.github.com>
2024-01-17 11:33:44 +0900

    [level 3] Title: 합승 택시 요금, Time: 0.90 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -1,17 +1,56 @@
+from heapq import heappush, heappop
+from collections import defaultdict
+
 def solution(n, s, a, b, fares):
-    distances = [[0 if i == j else float('inf') for j in range(n)] for i in range(n)]
+    distances = {
+        s-1: [float('inf')] * n,
+        a-1: [float('inf')] * n,
+        b-1: [float('inf')] * n
+    }
     
+    graph = defaultdict(list)
     for c, d, f in fares:
-        distances[c-1][d-1] = f
-        distances[d-1][c-1] = f
+        graph[c-1].append((d-1, f))
+        graph[d-1].append((c-1, f))
     
-    for k in range(n):
-        for i in range(n):
-            for j in range(n):
-                distances[i][j] = min(distances[i][j], distances[i][k] + distances[k][j])
+    q = [(0, s-1)]
+    distances[s-1][s-1] = 0
+    while q:
+        current_distance, current_node = heappop(q)
+        if current_distance > distances[s-1][current_node]:
+            continue
+            
+        for neighbor_node, distance in graph[current_node]:
+            if current_distance + distance < distances[s-1][neighbor_node]:
+                heappush(q, (current_distance + distance, neighbor_node))
+                distances[s-1][neighbor_node] = current_distance + distance
+                
+    q = [(0, a-1)]
+    distances[a-1][a-1] = 0
+    while q:
+        current_distance, current_node = heappop(q)
+        if current_distance > distances[a-1][current_node]:
+            continue
+            
+        for neighbor_node, distance in graph[current_node]:
+            if current_distance + distance < distances[a-1][neighbor_node]:
+                heappush(q, (current_distance + distance, neighbor_node))
+                distances[a-1][neighbor_node] = current_distance + distance
+                
+    q = [(0, b-1)]
+    distances[b-1][b-1] = 0
+    while q:
+        current_distance, current_node = heappop(q)
+        if current_distance > distances[b-1][current_node]:
+            continue
+            
+        for neighbor_node, distance in graph[current_node]:
+            if current_distance + distance < distances[b-1][neighbor_node]:
+                heappush(q, (current_distance + distance, neighbor_node))
+                distances[b-1][neighbor_node] = current_distance + distance    
                 
     answer = float('inf')
     for i in range(n):
-        answer = min(answer, distances[s-1][i] + distances[i][a-1] + distances[i][b-1])
+        answer = min(answer, distances[s-1][i] + distances[a-1][i] + distances[b-1][i])
         
     return answer 
