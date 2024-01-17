bd23aa2b4df772188fcc3a0ced9b528a29010f67
Yein Lee (이예인) <39490214+yein-lee@users.noreply.github.com>
2024-01-17 11:10:02 +0900

    [level 3] Title: 합승 택시 요금, Time: 292.74 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -1,30 +1,17 @@
-from heapq import heappop, heappush
-
-    
 def solution(n, s, a, b, fares):
-    INF = 2e9
-    graph = [[INF for _ in range(n+1)] for _ in range(n+1)]
+    distances = [[0 if i == j else float('inf') for j in range(n)] for i in range(n)]
     
-    for n1, n2, w in fares:
-        graph[n1][n2] = w
-        graph[n2][n1] = w
-
-        
-    for i in range(1, n+1):
-        graph[i][i] = 0
-
-    for k in range(1, n+1):
-        for i in range(1, n+1):
-            for j in range(1, n+1):
-                graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
-
-    answer = INF
-    for i in range(1, n+1):
-        answer = min(answer, graph[s][i] + graph[i][a] + graph[i][b])
-                    
-    answer = INF
-    for i in range(1, n+1):
-        answer = min(graph[s][i] + graph[i][a] + graph[i][b], answer)
-        
+    for c, d, f in fares:
+        distances[c-1][d-1] = f
+        distances[d-1][c-1] = f
+    
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                distances[i][j] = min(distances[i][j], distances[i][k] + distances[k][j])
+                
+    answer = float('inf')
+    for i in range(n):
+        answer = min(answer, distances[s-1][i] + distances[i][a-1] + distances[i][b-1])
         
     return answer 
