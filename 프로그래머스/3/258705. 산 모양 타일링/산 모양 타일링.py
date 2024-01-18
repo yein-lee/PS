3df95f9f3c85fb5a7f41e277454bb45a02856292
Yein Lee (이예인) <39490214+yein-lee@users.noreply.github.com>
2024-01-18 12:41:32 +0900

    [level 3] Title: 산 모양 타일링, Time: 30.81 ms, Memory: 15.3 MB -BaekjoonHub

--- 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.py
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.py
@@ -1,29 +1,21 @@
-from collections import deque
-
-
 def solution(n, tops):
     tiles = [0] * (2*n+1)
     for i, top in enumerate(tops):
         if top == 1:
             tiles[i*2+1] = 1
-
-    answer = 0
-    stack = [0] 
-
-    while stack:
-        current_index = stack.pop()
-
-        if current_index >= 2*n:
-            answer += 1
-            continue
-
-        if tiles[current_index] == 1:
-            stack.append(current_index + 1)
-            stack.append(current_index + 1)
-            stack.append(current_index + 2)
-        elif tiles[current_index] == 0:
-            stack.append(current_index + 1)
-            stack.append(current_index + 2)
             
-    return answer % 10007    
-    
+    dp = [0] * (2*n + 1)
+    dp[0] = 1 
+    if tiles[1] == 1:
+        dp[1] = 3
+    else:
+        dp[1] = 2
+
+    for i in range(2, 2*n + 1):
+        if tiles[i] == 1:
+            dp[i] = (dp[i-2] + dp[i-1] *2) % 10007
+        else:
+            dp[i] = (dp[i-1] + dp[i-2]) % 10007
+            
+
+    return dp[-1]
